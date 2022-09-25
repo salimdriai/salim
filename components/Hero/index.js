e2a396a7ae7e133ea0e74f0e9e2713bc1b21fb0d
salim driai <salimdriai15@gmail.com>
2022-09-25 16:55:36 +0100

    add favicon and fix title

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className="hero-content flex-col lg:flex-row">
         <img ref={parallax.ref} src="/space.svg" className="max-w-sm " />
         <div>
-          <p className="text-2xl font-bold">Hi there !</p>
+          <p className="text-2xl ">Hi there !</p>
           <h1 className="font-bold">I&apos;m Salim.</h1>
           <p className="py-4 text-center">
             A passionate, ambitious & self taught software developer.
