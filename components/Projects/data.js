00b8c063d93dcf54a9807edb35b7023982ffe131
salim driai <salimdriai15@gmail.com>
2022-09-26 19:14:20 +0100

    fix structure

---
+++ components/Projects/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: 'ActiList',
+    description:
+      'A secret NFT auction platform built on secret network blockchain.',
+    src: '/logo-acti.png',
+    link: 'https://www.test.actilist.co',
+    role: 'full stack dev',
+  },
+  {
+    title: 'Eland',
+    description:
+      'Multi users property management platform built with Next.js & Typescrpit.',
+    src: '/eland-logo-04.png',
+    link: 'https://www.eland.pk',
+    role: 'front end dev',
+  },
+  {
+    title: 'Enviroblocks',
+    description:
+      'Gaming web application on top of the WAX blockchain built with React.',
+    src: '/enviro-logo.png',
+    link: 'https://www.enviroblocks.io',
+    role: 'front end dev',
+  },
+]
---
+++ components/Skills/data.js
@@ -0,0 +1,65 @@
+export const skills = [
+  {
+    label: 'Javascript',
+    src: '/icons8-javascript.svg',
+    percentage: 90,
+    text: 'My proffession',
+  },
+  {
+    label: 'React ',
+    src: '/icons8-react.svg',
+    percentage: 85,
+    text: 'I also do Next.js',
+  },
+  {
+    label: 'AWS',
+    src: '/icons8-amazon-web-services.svg',
+    percentage: 60,
+    text: 'Comfortable with Amplify',
+  },
+  {
+    label: 'Node.js',
+    src: '/icons8-nodejs.svg',
+    percentage: 70,
+    text: 'Something i love',
+  },
+  {
+    label: 'C++',
+    src: '/icons8-cpp.svg',
+    percentage: 50,
+    text: 'To solve challenges',
+  },
+  {
+    label: 'Web3',
+    src: '/icons8-ethereum.svg',
+    percentage: 50,
+    text: 'Used to blockchain',
+  },
+  {
+    label: 'Typescript',
+    src: '/icons8-typescript.svg',
+    percentage: 50,
+    text: 'Types are cool',
+  },
+]
+
+export const tools = [
+  {
+    label: 'Git',
+    src: '/icons8-git.svg',
+    percentage: 80,
+    text: 'This is so important',
+  },
+  {
+    label: 'Slack',
+    src: '/icons8-slack-new.svg',
+    percentage: 90,
+    text: 'A team player',
+  },
+  {
+    label: 'Visual studio',
+    src: '/icons8-visual-studio-2019.svg',
+    percentage: 85,
+    text: 'Home',
+  },
+]
